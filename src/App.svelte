c5aab0882cc9b7cc99ffc199fe90babae051ebbb
Brian Bowman <seeker14491@gmail.com>
2021-09-05 04:20:37 -0500

    Better layout + title and GitHub link

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,15 @@
 	import { substatData } from "./data";
 </script>
 
-{#each substatData as sd}
-	<Chart substatData={sd} />
-{/each}
+<div class="flex flex-col items-center py-8">
+	<h1 class="text-2xl text-center">Genshin 5⭐ artifact subroll ranges</h1>
+	<div class="grid xl:grid-cols-2 w-full gap-8 p-4">
+		{#each substatData as sd}
+			<Chart substatData={sd} />
+		{/each}
+	</div>
+	<a href="https://github.com/Seeker14491/genshin-substat-chart" target="_blank" class="underline pt-8">GitHub</a>
+</div>
 
 <style global>
 	@tailwind base;
